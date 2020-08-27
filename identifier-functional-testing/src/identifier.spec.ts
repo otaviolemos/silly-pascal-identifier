c2af46a3a4406323c76549b6b3c77c1e30c4ec50
Otavio Lemos <otaviolemos@gmail.com>
2020-08-27 17:31:20 -0300

    test: correctly handles identifiers with invalid first characters and remaining characters

--- identifier-functional-testing/src/identifier.spec.ts
+++ identifier-functional-testing/src/identifier.spec.ts
@@ -1,5 +1,23 @@
+import { isValid } from './identifier'
+
+// ("1234", false) - Starts with a letter
+// ("a12^34", false) - Contains only letters or digits
+// ("", false) - len(S) = 0
+// ("a12345678", false) - len(S) > 6
+// (null, false) - String is null
+// ("a1234", true)
+
 describe('Identifier validator', () => {
-  test('should return false if identifier exceeds specified length', () => {
-    expect(true).toEqual(true)
+  test('should return true if string is a valid identifier', () => {
+    const ret = isValid('a1234')
+    expect(ret).toEqual(true)
+  })
+  test('should return false if string does not start with a letter', () => {
+    const ret = isValid('1234')
+    expect(ret).toEqual(false)
+  })
+  test('should return false if string contains invalid character', () => {
+    const ret = isValid('a12^34')
+    expect(ret).toEqual(false)
   })
 })
