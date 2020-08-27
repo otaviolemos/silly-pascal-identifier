9458945183a15ddeb0b95c1e3fc988e4be2de23b
Otavio Lemos <otaviolemos@gmail.com>
2020-08-27 17:55:07 -0300

    refactor: add spaces between tests

--- identifier-functional-testing/src/identifier.spec.ts
+++ identifier-functional-testing/src/identifier.spec.ts
@@ -12,30 +12,37 @@ describe('Identifier validator', () => {
     const ret = isValid('a1234')
     expect(ret).toEqual(true)
   })
+
   test('should return false if string does not start with a letter', () => {
     const ret = isValid('1234')
     expect(ret).toEqual(false)
   })
+
   test('should return false if string contains invalid character', () => {
     const ret = isValid('a12^34')
     expect(ret).toEqual(false)
   })
+
   test('should return false if string is empty', () => {
     const ret = isValid('')
     expect(ret).toEqual(false)
   })
+
   test('should return false if string exceeds valid length', () => {
     const ret = isValid('a12345678')
     expect(ret).toEqual(false)
   })
+
   test('should return false if string is null', () => {
     const ret = isValid(null)
     expect(ret).toEqual(false)
   })
+
   test('should return true with valid identifier with length 1', () => {
     const ret = isValid('i')
     expect(ret).toEqual(true)
   })
+
   test('should return true with valid identifier with length 6', () => {
     const ret = isValid('a12345')
     expect(ret).toEqual(true)
