4f6a45bb9701cea9793bbd142cf86eba12acb323
Otavio Lemos <otaviolemos@gmail.com>
2020-08-27 17:37:21 -0300

    feat: correctly handles string exceeding valid length"

--- identifier-functional-testing/src/identifier.ts
+++ identifier-functional-testing/src/identifier.ts
@@ -1,5 +1,5 @@
 export function isValid (identifier: string): boolean {
-  if (identifier.length < 1) {
+  if (identifier.length < 1 || identifier.length > 6) {
     return false
   }
   if (!/^[a-zA-Z]+$/.test(identifier.charAt(0)) ||
