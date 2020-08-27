0e03feea6008f049fe57fae85f964ee7fa137ece
Otavio Lemos <otaviolemos@gmail.com>
2020-08-27 17:34:23 -0300

    feat: correctly handles empty strings

--- identifier-functional-testing/src/identifier.ts
+++ identifier-functional-testing/src/identifier.ts
@@ -1,4 +1,7 @@
 export function isValid (identifier: string): boolean {
+  if (identifier.length < 1) {
+    return false
+  }
   if (!/^[a-zA-Z]+$/.test(identifier.charAt(0)) ||
       !/^[0-9a-zA-Z]+$/.test(identifier)) {
     return false
