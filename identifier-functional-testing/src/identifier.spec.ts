307ad7a585b9dd3f24cdc75a923ffb13ad625612
Otavio Lemos <otaviolemos@gmail.com>
2020-08-27 18:14:04 -0300

    docs: added comment to explain equivalence classes

--- identifier-functional-testing/src/identifier.spec.ts
+++ identifier-functional-testing/src/identifier.spec.ts
@@ -1,11 +1,12 @@
 import { isValid } from './identifier'
 
+// Test cases created to cover equivalence classes:
 // ("1234", false) - Starts with a letter
 // ("a12^34", false) - Contains only letters or digits
 // ("", false) - len(S) = 0
 // ("a12345678", false) - len(S) > 6
 // (null, false) - String is null
-// ("a1234", true)
+// ("a1234", true) - valid identifier
 
 describe('Identifier validator', () => {
   test('should return true if string is a valid identifier', () => {
